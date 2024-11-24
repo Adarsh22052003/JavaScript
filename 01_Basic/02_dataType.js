3dad92ea46bd73cf9bbc648feeb9e9e3da45ee89
adarshsachan <adarshsachanhiet@gmail.com>
2024-11-25 04:52:25 +0530

    dataType

---
+++ 01_Basic/02_dataType.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Hello"); // we are using nodejs, not browser
+
+// code readability should be high
+
+let  name = 'adasrsh';
+let age = 38;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/flase
+// null
+// undefined =>
+// symbol
+
+
+
+// Object
+
+console.log(typeof undefined);// undefined
+console.log(typeof null);// object
+

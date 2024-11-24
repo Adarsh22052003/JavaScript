cad93e6b9d8e89cdcb4096fcb45943edf1c92222
adarshsachan <adarshsachanhiet@gmail.com>
2024-11-25 05:21:55 +0530

    Operations

--- 01_Basic/03_dataTypeConversion.js
+++ 01_Basic/03_dataTypeConversion.js
@@ -1,30 +1,62 @@
-// let score = "33abc";
-// let score = null;
-// let score = undefined;
-let score = "Hello";
+// // let score = "33abc";
+// // let score = null;
+// // let score = undefined;
+// let score = "Hello";
 
-// "33" => 33
-// "3dff" => Nan
-// true => 1; false => 0
+// // "33" => 33
+// // "3dff" => Nan
+// // true => 1; false => 0
 
-let isLoggedIn = 1;
-let booleanIsLoggedIn = Boolean(isLoggedIn);
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
-// 1 => true; 0=>false
-// "" => false
-// "ankur" => true
+// // 1 => true; 0=>false
+// // "" => false
+// // "ankur" => true
  
-console.log(typeof(score));
+// console.log(typeof(score));
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber)
-console.log(valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+// 8*************************Operations***********************
+let value = 3
+let negativeValue = -value;
+// console.log(negativeValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+
+let str1 = "Hello"
+let str2 = " world"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);

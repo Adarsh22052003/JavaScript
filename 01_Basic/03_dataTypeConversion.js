5cd60799cb87519cc5f4d27f9e453a789498983a
adarshsachan <adarshsachanhiet@gmail.com>
2024-11-25 05:06:37 +0530

    conversion of dataType

---
+++ 01_Basic/03_dataTypeConversion.js
@@ -0,0 +1,30 @@
+// let score = "33abc";
+// let score = null;
+// let score = undefined;
+let score = "Hello";
+
+// "33" => 33
+// "3dff" => Nan
+// true => 1; false => 0
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0=>false
+// "" => false
+// "ankur" => true
+ 
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
